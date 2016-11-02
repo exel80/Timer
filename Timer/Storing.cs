7a266988434dc99cf0d5d53981ac6c5632a3887e
Exel80 <exel80p@gmail.com>
2016-11-02 22:42:38 +0200

    Version 1.0.1 * Now saving settings when hit start. * Added Newtonsoft.Json.dll

---
+++ Timer/Storing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Timer
+{
+    class Storing
+    {
+        // Set timer
+        public int hours { get; set; } 
+        public int minutes { get; set; }
+        public int seconds { get; set; }
+
+        // Output
+        public string path { get; set; }
+        public string format { get; set; }
+    }
+}
--- Timer/Timer.cs
+++ Timer/Timer.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+
+using Newtonsoft.Json;
 
 namespace Timer
 {
@@ -17,6 +21,9 @@ namespace Timer
         {
             InitializeComponent();
             updateTimerLabels();
+
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "/Settings.json"))
+                readData();
         }
 
         #region Numerics
@@ -48,6 +55,8 @@ namespace Timer
                 MessageBox.Show("Output path can't be null!");
                 return;
             }
+
+            writeData();
 
             CooldownTimer.Start();
             CooldownTimer.Enabled = true;
@@ -108,6 +117,10 @@ namespace Timer
             {
                 CooldownTimer.Stop();
                 CooldownTimer.Enabled = false;
+
+                buttonStart.Enabled = true;
+                buttonPause.Enabled = false;
+                buttonStop.Enabled = false;
             }
         }
         private void updateOutput(string path)
@@ -145,11 +158,43 @@ namespace Timer
                     Hours.Text = numericHours.Value.ToString();
             }
         }
-        #endregion
-
         private void label2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("{0} = Time will be print here.\n^ This must be included in format! Other ways timer will not been shown!\n\nHere is one example format what you can try out:\nStream start soon, {0}!");
         }
+        #endregion
+
+        #region JSON
+        private void writeData()
+        {
+            Storing _storing = new Storing();
+
+            _storing.hours = Convert.ToInt32(numericHours.Value);
+            _storing.minutes = Convert.ToInt32(numericMinutes.Value);
+            _storing.seconds = Convert.ToInt32(numericSeconds.Value);
+
+            _storing.path = ouputText.Text;
+            _storing.format = Format.Text;
+
+            Debug.WriteLine(JsonConvert.SerializeObject(_storing));
+
+            using (StreamWriter file = File.CreateText(AppDomain.CurrentDomain.BaseDirectory + "/Settings.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, _storing);
+            }
+        }
+        private void readData()
+        {
+            Storing _storing = JsonConvert.DeserializeObject<Storing>(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Settings.json"));
+
+            numericHours.Value = _storing.hours;
+            numericMinutes.Value = _storing.minutes;
+            numericSeconds.Value = _storing.seconds;
+
+            ouputText.Text = _storing.path;
+            Format.Text = _storing.format;
+        }
+        #endregion
     }
 }
